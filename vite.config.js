22e6d988b214a1c43ed73d617a373b981b38d587
Vitalii Vasylets <mohara88s@gmail.com>
2024-11-13 13:30:54 +0200

    deleted base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/cll-app2/",
   build: {
     sourcemap: true,
   }
