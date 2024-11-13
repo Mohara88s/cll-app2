12d9555449350657c1848ddabd274f6c2a7e859a
Vitalii Vasylets <mohara88s@gmail.com>
2024-11-13 13:25:54 +0200

    added base

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/cll-app2/",
   build: {
     sourcemap: true,
   }
